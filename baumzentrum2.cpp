d856d1f8f5240cec9bd5365728759577abeaea05
victoriadfs <84072235+victoriadfs@users.noreply.github.com>
2022-11-11 08:11:06 +0100

    Add files via upload
    Mit Queue<pair<int,int>>

--- baumzentrum2.cpp
+++ baumzentrum2.cpp
@@ -1,5 +1,9 @@
 //Programm soll das Zentrum eines Baumes bestimmen 
 //Algorithmus basierend auf Beweis VL zum finden eines Zentrums in einem Baum
+/*Bemerkung zur Kompilierung:
+Folgende Komandos wurden benutzt (Hier am Beispiel mit inst8_):
+clang++ -std=c++11 -o test baumzentrum2.cpp graph.cpp
+./test inst8_*/
 #include <iostream>
 #include<stdio.h>
 #include<queue>
@@ -8,35 +12,39 @@
 
 using namespace std;
 
-void zentrumbestimmen(Graph g){
+void zentrumbestimmen(const Graph &g){
     int n=g.num_nodes();
     vector<int>grad(n,0); //Speichern Grad jedes Knoten
-    queue<int>schlange; //Speichern Blaetter
+    queue<pair<int,int>>schlange; //Speichern Blaetter_id,Phase elimination
     for(int i=0;i<n;i++){
         grad[i]=g.get_node(i).adjacent_nodes().size();
-        if(grad[i]==1)schlange.push(i);
+        if(grad[i]==1)schlange.push(make_pair(i,0));
     }
     //Beginne ein modifiziertes BFS
     vector<bool>visited(g.num_nodes(),false);
-    vector<int>level(n,0);//in welchem Schritt von entferne alle Blaetter wird der jeweilige Knoten entfernt
-    int levelzentrum=0;// wird als letztes entfernt
-    while(!schlange.empty()){ //jeder Knoten kommt genau ein mal in der Schleife vor 
-        int knoten=schlange.front();
+    //Modifiziertes BFS
+    while(n>2){ //jeder Knoten kommt genau ein mal in der Schleife vor 
+        int knoten=schlange.front().first;
+        int nextphase=schlange.front().second+1;//geben zuruck was fuer ein Level die naechsten eliminierten Knoten haben werden
         schlange.pop();
+        n--;
         visited[knoten]=true;
-        for(auto neighbor : g.get_node(knoten).adjacent_nodes()){
+        for(auto neighbor : g.get_node(knoten).adjacent_nodes()){//jede Kante wird hoechstens 2 Mal aufgerufen
             int n_id=neighbor.id();
-            grad[n_id]--;//entferne den Knoten
+            grad[n_id]--;//entferne der Kante 
             if(!visited[n_id] and grad[n_id]==1){//wenn der Nachbar zum Blatt geworden ist fuege hinzu
-                schlange.push(n_id);
-                level[n_id]=level[knoten]+1;
-                levelzentrum=max(level[n_id],levelzentrum);
+                schlange.push(make_pair(n_id,nextphase));
             }
         }
     }
-    for(int i=0;i<n;i++){
-        if(level[i]==levelzentrum)cout<<i<<" ";
-    }
+    //Nur zwei Knoten wurden nicht eliminiert,aber sie sind in der queue denn alle ihre Nachbarn wurden eliminiert ausser vllt einen und damit sind sie Blaetter
+    int poswurzel1=schlange.front().first;
+    int phase1=schlange.front().second;
+    schlange.pop();
+    int poswurzel2=schlange.front().first;
+    int phase2=schlange.front().second;
+    if(phase1==phase2)cout<<poswurzel1<<" "<<poswurzel2;//wenn im selben eliminaritionschritt dann sind beide Wurzeln
+    else cout<<poswurzel2;//sonst ist der letzte die Wurzel, denn die Phasen werden geordnet im queue aufgerufen
 }
 
 int main(int argc, char* argv[])
